333a5ee7df7ad1ada56e46e65089c04f02eb54db
dani <mdanielm@protonmail.com>
2018-12-10 18:11:22 +0100

    printing api initial commit

---
+++ webplatform/settings_secret.py
@@ -0,0 +1,18 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Email
+'''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'mail.fi.upm.es'
+EMAIL_HOST_USER = 'tryit.da'
+EMAIL_HOST_PASSWORD = 'ti1926'
+'''
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'taquillasdafi'
+EMAIL_HOST_PASSWORD = 'kukuruchodk4hoots'
+DEFAULT_FROM_EMAIL = 'taquillasdafi'
+DEFAULT_TO_EMAIL = 'd.mmoreno@alumnos.upm.es'
+
